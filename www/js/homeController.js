3f9856d1bc5ea2845cf7393732e5fdd4889e2ea1
crystal <sujeong.bae@dmajor.kr>
2015-09-08 11:40:41 +0900

    added comment function

--- www/js/homeController.js
+++ www/js/homeController.js
@@ -8,6 +8,7 @@ wcm.controller("HomeController", function($scope, $state, $cordovaCamera, $http,
 
   $scope.page = 0;
   $scope.cards = [];
+  $scope.post_id = [];
 
   if (window.localStorage['user'] != null) {
 
@@ -22,6 +23,7 @@ wcm.controller("HomeController", function($scope, $state, $cordovaCamera, $http,
     console.log('Refreshing!');
 
     $timeout( function() {
+
       var request = $http({
           method: "get",
           url: mServerAPI + "/card/" + $scope.page,
@@ -32,11 +34,46 @@ wcm.controller("HomeController", function($scope, $state, $cordovaCamera, $http,
 
       /* Successful HTTP post request or not */
       request.success(function(data) {
+
           for (var i = 0; i <  data.cards.length; i++) {
               var object =  data.cards[i];
-              // console.log(object.id + ' - ' + object.title + " " + object.description);
               $scope.cards.push(object);
+
+              // /* get comments */
+              // var request2 = $http({
+              //     method: "get",
+              //     url: mServerAPI + "/comment/" + object.id,
+              //     crossDomain : true,
+              //     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+              //     cache: false
+              
+              // });       
+                
+              // console.log(object.id);
+      
+              // request2.success(function(data) {
+
+              //   /* post comments length */
+              //   var comments_count = parseInt(data.comments.length);
+              //   var formData = { comments_count: comments_count };
+              //   var postData = 'commentData='+JSON.stringify(formData);
+
+              //   var request3 = $http({
+              //       method: "post",
+              //       url: mServerAPI + "/cardDetail/" + object.id,
+              //       crossDomain : true,
+              //       data: postData,
+              //       headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+              //       cache: false
+              //   });
+              //   /* post comments length end */
+
+              // });
+
+              // /* get comments end */
           }
+          
+
           $scope.page++;
           $scope.$broadcast('scroll.infiniteScrollComplete');
       });
--- www/js/postController.js
+++ www/js/postController.js
@@ -2,6 +2,8 @@ wcm.controller("PostController", function($scope, $http, $stateParams) {
   
 	$scope.postId = $stateParams.postId;
   $scope.cards = [];
+  $scope.comments = [];
+  $scope.comments_count = 0;
 
   var request = $http({
     method: "get",
@@ -20,6 +22,10 @@ wcm.controller("PostController", function($scope, $http, $stateParams) {
     $scope.lng = data.cards[0].location_long;
     $scope.initMap(data);
   });
+
+
+
+  // ==================================== reverse geocording ======================================
 
   $scope.initMap = function(data) {
 	  var map = new google.maps.Map(document.getElementById('map'), {
@@ -64,8 +70,111 @@ wcm.controller("PostController", function($scope, $http, $stateParams) {
 	}
 
 
+  // ==================================== get comments ======================================
+
+  var request2 = $http({
+      method: "get",
+      url: mServerAPI + "/comments",
+      crossDomain : true,
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+      cache: false
+  });
+
+  /* Successful HTTP post request or not */
+  request2.success(function(data) {
+
+      for (var i = 0; i <  data.comments.length; i++) {
+        var object =  data.comments[i];
+       	
+       	if (object.post_id === $stateParams.postId) {
+
+       		if (object.username === null || object.userimage === null) {
+       			object.username = "no nickname";
+       			object.userimage = "http://mud-kage.kakao.co.kr/14/dn/btqchdUZIl1/FYku2ixAIgvL1O50eDzaCk/o.jpg"
+       			$scope.comments.push(object);
+       			$scope.comments_count ++;
+       		} else {
+       			$scope.comments.push(object);
+       			$scope.comments_count ++;
+       		}
+       	}    
+      }
+  });
+
+
+  // ==================================== post comment ======================================
+
+	$scope.addComment =function() {
+  
+    var comment = document.getElementById("comment").value
+
+    if (window.localStorage['user'] != null) {
+	    var user = JSON.parse(window.localStorage['user'] || '{}');
+	    $scope.username = user.properties.nickname;
+	    $scope.userimage = user.properties.thumbnail_image;
+	  }
+
+	  var post_id = parseInt($stateParams.postId);
+    var user_app_id = parseInt(user.id);
+    var content = comment;
+    var username = $scope.username;
+    var userimage = $scope.userimage;
+
+    var formData = {
+		      						post_id: post_id,
+		                  user_app_id: user_app_id,
+		                  content: content,
+		                  username: username,
+		                  userimage: userimage
+		          			};
+
+    var postData = 'commentData='+JSON.stringify(formData);
+
+
+    $scope.comments.push(formData);
+ 		console.log($scope.comments);
+
+
+    var request = $http({
+        method: "post",
+        url: mServerAPI + "/comments",
+        crossDomain : true,
+        data: postData,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+        cache: false
+    });
+
+    /* Successful HTTP post request or not */
+    request.success(function(data) {
+
+    	document.getElementById("comment").value = "";
+    	$scope.comments_count ++;
+
+    });
+  }
+
 
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
